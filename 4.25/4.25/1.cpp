a90a1526d307ac7b5571aa75ed1a1d9478968233
詹昊 <2352281364@qq.com>
2021-04-25 10:40:16 +0800

    登录密码验证

---
+++ 4.25/4.25/1.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<Windows.h>
+int main()
+{
+	int i=0;
+	char password[20]={0};
+	for(i=0;i<3;i++)
+	{
+		printf("ÇëÊäÈëÃÜÂë:");
+		scanf("%s",password);
+		if(strcmp(password,"123456")==0)
+		{
+			printf("µÇÂ¼³É¹¦\n");
+			break;
+		}
+		else{printf("ÃÜÂë´íÎó\n");}
+	}
+	if (i==3)
+		printf("Èý´ÎÃÜÂë¾ù´íÎó\n");
+	return 0;
+}
