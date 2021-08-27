82f299d05c4bbec6356825092e8c17e15f740e0a
詹昊 <2352281364@qq.com>
2021-08-27 17:05:14 +0800

    8.26日学习

---
+++ 8.26/源.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define INT_PTR int*
+typedef int* int_ptr;
+INT_PTR a, b;
+int_ptr c, d;
+#include<stdio.h>
+//int main() {
+//	int a[] = { 2,4,6,8,10,12,14,16,18,20,22,24 }, * q[4], k;
+//	for (k = 0; k < 4; k++) {
+//		q[k] = &a[k * 3];
+//	}
+//	printf("%d\n", q[3][1]);
+//}
+
+
+
+//int main()
+//{
+//	int i, j, a = 0;
+//	for (i = 0; i < 2; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			if (j % 2)
+//				break;
+//			a++;
+//		} a
+//			++;
+//	} 
+//	printf("%d\n", a);
+//	
+//}
+//int main() {
+//	int f;
+//	a = &f;
+//	
+//}
