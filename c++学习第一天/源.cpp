8970b7b5f4947342979f944ed9b716bf9edeac50
詹昊 <2352281364@qq.com>
2021-08-27 17:04:52 +0800

    第一天学习c++

---
+++ c++学习第一天/源.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+#include<string>
+//
+// 常量：
+// #define 宏常量
+// const修饰的变量
+// 
+
+//int main() {
+//	cout << "hello c++" << endl;
+//	return 0;
+//}
+
+
+//int main() {
+//	//变量
+//	int a = 10;
+//	cout << "a=" << a << endl;
+//	return 0;
+//}
+
+
+//int main() {
+//	char a = 'a';
+//	cout << (int)a << endl;
+//	return 0;
+//}
+
+
+int main() {
+	string str = "hello world";
+	cout << str << endl;
+	int a;
+	cin >> a;
+	cout << a << endl;
+	return 0;
+}
